03e32db1c89c0470ad520bcad3b517f7bd81d155
jnsmtnr <janosmaitner@gmail.com>
2021-12-05 18:50:44 +0100

    day 5

---
+++ day05/puzzle10.js
@@ -0,0 +1,40 @@
+const { readFileSync } = require("fs")
+
+const input = readFileSync('input.txt', 'utf8')
+
+const lines = input.split('\n').filter(Boolean).map(line => line.replace(' -> ', ',').split(',').map(Number))
+
+const map = {}
+
+for (const line of lines) {
+    const [x1, y1, x2, y2] = line
+
+    if (x1 !== x2 && y1 !== y2) {
+        for (let x = x1, y = y1; (x1 < x2) ? (x <= x2): (x >= x2) ; (x1 < x2) ? x++ : x--, (y1 < y2) ? y++ : y--) {
+            if (map[x + '-' + y]) map[x + '-' + y]++
+            else map[x + '-' + y] = 1
+        }
+    }
+
+    if (x1 === x2) {
+        for (let y = Math.min(y1, y2); y <= Math.max(y1, y2); y++) {
+            if (map[x1 + '-' + y]) map[x1 + '-' + y]++
+            else map[x1 + '-' + y] = 1
+        }
+    }
+
+    if (y1 === y2) {
+        for (let x = Math.min(x1, x2); x <= Math.max(x1, x2); x++) {
+            if (map[x + '-' + y1]) map[x + '-' + y1]++
+            else map[x + '-' + y1] = 1
+        }
+    }
+}
+
+let danger = 0
+
+for (const point of Object.values(map)) {
+    if (point > 1) danger++
+}
+
+console.log(danger)
